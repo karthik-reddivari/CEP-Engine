852ee915c090d99f46c3bf9433fedb86b009e64d
Karthik Reddivari <karthik2r@gmail.com>
2020-03-16 19:50:54 +0000

    Initial Project Setup

---
+++ src/main/java/io/karthik/cepengine/CepEngineApplication.java
@@ -0,0 +1,13 @@
+package io.karthik.cepengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/karthik/cepengine/CepEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.karthik.cepengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
